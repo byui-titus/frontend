450854e1180b05eaf6b2a7f07cee482479dc63bf
byui-titus <titusrayz33@gmail.com>
2025-10-08 02:24:46 +0300

    done

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,12 @@
 import { fileURLToPath } from 'url';
 import { dirname, resolve } from 'path';
 import { defineConfig } from 'vite';
-import eslint from 'vite-plugin-eslint';
 
 const __filename = fileURLToPath(
     import.meta.url);
 const __dirname = dirname(__filename);
 
 export default defineConfig({
-    plugins: [
-        eslint({
-            // ESLint configuration
-            lintOnStart: true, // Lint on start
-            formatter: 'stylish', // Specify formatter
-        }),
-    ],
     root: "src",
     build: {
         outDir: "../dist",
