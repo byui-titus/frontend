a4116445528868a2bbeb905d0a6f7c266c436967
byui-titus <titusrayz33@gmail.com>
2025-10-11 12:25:42 +0300

    login

--- src/js/authGuard.js
+++ src/js/authGuard.js
@@ -3,6 +3,6 @@ export function requireAuth() {
     const token = localStorage.getItem('token');
     if (!token) {
         alert('Please login to continue.');
-        window.location.href = 'login/login.html';
+        window.location.href = '/login/login.html';
     }
 }
